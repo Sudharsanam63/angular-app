99307231cfef2a6623c164035fa4889a15653d17
Sudharsanam63 <122216517+Sudharsanam63@users.noreply.github.com>
2024-08-06 08:23:26 +0530

    backend connection

---
+++ database/mongoose.js
@@ -0,0 +1,15 @@
+const mongoose=require('mongoose');
+
+//For Asynchronous operations 
+mongoose.Promise=global.Promise;
+
+mongoose.connect('mongodb://127.0.0.1:27017/taskmanagerdb')
+.then(()=>{
+    console.log('Database connected')
+})
+.catch((error)=>{
+    console.log(error)
+});
+
+module.exports =mongoose
+
